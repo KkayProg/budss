8a9e0723cb4a37cdfa660586a810659a0a71fa01
KkayProg <kirillonucin63@gmail.com>
2025-02-13 04:24:07 +0600

    added logic for form

--- scripts/index.js
+++ scripts/index.js
@@ -41,7 +41,31 @@ closeburgerBtn.addEventListener("click", () => {
 });
 
 
-// modal Window
+
+// sticky header
+document.addEventListener("DOMContentLoaded", function () {
+    let headerMenuRight = document.querySelector(".header__menu-right");
+    let headerMenuLeft = document.querySelector(".header__menu-left");
+    let header = document.querySelector(".header");
+
+    let headerHeight = header.offsetHeight;
+
+    function updateHeader() {
+        if (window.scrollY > 38) {
+            headerMenuRight.classList.add("sticky");
+            headerMenuLeft.classList.add("hidden");
+            document.body.style.paddingTop = `${headerHeight}px`;
+        } else {
+            headerMenuRight.classList.remove("sticky");
+            headerMenuLeft.classList.remove("hidden");
+            document.body.style.paddingTop = "0px";
+        }
+    }
+
+    updateHeader();
+    window.addEventListener("scroll", updateHeader);
+});
+
 const modalTriggers = document.querySelectorAll(".btn");
 const windowInnerWidth = document.documentElement.clientWidth;
 const scrollbarWidth = parseInt(window.innerWidth) - parseInt(windowInnerWidth);
@@ -88,68 +112,158 @@ document.addEventListener("keydown", function (event) {
     if (event.key === "Escape" && modalBackground.style.display === "block") {
         modalBackground.style.display = "none";
         if (window.innerWidth >= 1366) {
-            bodyElementHTML.style.marginRight = "0";
+            bodyMargin();
         }
     }
 });
 
-// sticky header
-document.addEventListener("DOMContentLoaded", function () {
-    let headerMenuRight = document.querySelector(".header__menu-right");
-    let headerMenuLeft = document.querySelector(".header__menu-left");
-    let header = document.querySelector(".header");
+const formState = {
+    isNameValid: false,
+    isEmailValid: false,
+    isPhoneValid: false,
+};
 
-    let headerHeight = header.offsetHeight;
+const wrapperName = document.querySelector('.wrapper-name');
+const wrapperEmail = document.querySelector('.wrapper-email');
+const wrapperPhone = document.querySelector('.wrapper-phone');
+const inputName = document.querySelector('.input-name');
+const inputEmail = document.querySelector('.input-email');
+const inputPhone = document.querySelector('.modal__input-tel');
+const countrySelect = document.querySelector('.PhoneInputCountrySelect');
+const errorsFormText = document.querySelector('.errors-form');
+const submitBtn = document.querySelector(".modal__window__form-btn");
+const modalThanks = document.querySelector(".modal-thanks");
+const thanksClose = document.querySelector(".modal-thanks__close-btn");
+const thanksMainBtn = document.querySelector(".modal-thanks__btn");
 
-    function updateHeader() {
-        if (window.scrollY > 38) {
-            headerMenuRight.classList.add("sticky");
-            headerMenuLeft.classList.add("hidden");
-            document.body.style.paddingTop = `${headerHeight}px`;
-        } else {
-            headerMenuRight.classList.remove("sticky");
-            headerMenuLeft.classList.remove("hidden");
-            document.body.style.paddingTop = "0px";
-        }
+let isNameTouched = false;
+let isEmailTouched = false;
+let isPhoneTouched = false;
+
+function removeError(wrapper) {
+    wrapper.classList.remove('error-border');
+    const errorElement = wrapper.parentNode.querySelector('.error-message');
+    if (errorElement) {
+        errorElement.remove();
+    }
+}
+
+function addError(wrapper, message) {
+    wrapper.classList.add('error-border');
+
+    let errorElement = wrapper.parentNode.querySelector('.error-message');
+    if (!errorElement) {
+        errorElement = document.createElement("span");
+        errorElement.className = "error-message";
+        wrapper.parentNode.insertBefore(errorElement, wrapper.nextSibling);
+    }
+    errorElement.textContent = message;
+}
+
+function validateField(input, wrapper) {
+    if (!input.value.trim()) {
+        addError(wrapper, 'This field is required');
+        return false;
+    }
+    removeError(wrapper);
+    return true;
+}
+
+
+function updateFormState() {
+    if (isNameTouched) {
+        formState.isNameValid = validateField(inputName, wrapperName);
+    } else {
+        formState.isNameValid = false;
     }
 
-    updateHeader();
-    window.addEventListener("scroll", updateHeader);
+    if (isEmailTouched) {
+        formState.isEmailValid = validateField(inputEmail, wrapperEmail);
+    } else {
+        formState.isEmailValid = false;
+    }
+
+    if (isPhoneTouched) {
+        formState.isPhoneValid = validatePhone(inputPhone.value.replace(/\D/g, ''), countrySelect.value);
+    } else {
+        formState.isPhoneValid = false;
+    }
+
+    const isFormValid = formState.isNameValid && formState.isEmailValid && formState.isPhoneValid;
+
+    if (isFormValid) {
+        submitBtn.classList.add('active', 'btn');
+    } else {
+        submitBtn.classList.remove('active', 'btn');
+    }
+
+    const shouldShowErrors =
+        (isNameTouched && !formState.isNameValid) ||
+        (isEmailTouched && !formState.isEmailValid) ||
+        (isPhoneTouched && !formState.isPhoneValid);
+
+    errorsFormText.style.display = shouldShowErrors ? 'block' : 'none';
+
+    console.log('Форма валидна?', isFormValid);
+    return isFormValid;
+}
+
+inputName.addEventListener('focus', function () {
+    isNameTouched = true;
+    removeError(wrapperName);
+});
+inputName.addEventListener('blur', function () {
+    isNameTouched = true;
+    formState.isNameValid = validateField(inputName, wrapperName);
+    updateFormState();
 });
 
+inputEmail.addEventListener('focus', function () {
+    isEmailTouched = true;
+    removeError(wrapperEmail);
+});
+inputEmail.addEventListener('blur', function () {
+    isEmailTouched = true;
+    formState.isEmailValid = validateField(inputEmail, wrapperEmail);
+    updateFormState();
+});
 
-// modal window
-const phoneInput = document.querySelector(".modal__input-tel");
-const countrySelect = document.querySelector(".PhoneInputCountrySelect");
+inputPhone.addEventListener('focus', function () {
+    isPhoneTouched = true; 
+    removeError(wrapperPhone);
+});
+inputPhone.addEventListener('blur', function () {
+    isPhoneTouched = true;
+    formState.isPhoneValid = validatePhone(inputPhone.value.replace(/\D/g, ''), countrySelect.value);
+    updateFormState();
+});
+
+if (countrySelect.value === "RU") {
+    inputPhone.value = "+7";
+    inputPhone.addEventListener("input", handlePhoneInput);
+    inputPhone.addEventListener("keydown", handlePhoneKeydown);
+}
+
+countrySelect.addEventListener("change", function () {
+    resetValidationError();
+
+    inputPhone.value = "";
+    if (countrySelect.value === "RU") {
+        inputPhone.value = "+7";
+        inputPhone.addEventListener("input", handlePhoneInput);
+        inputPhone.addEventListener("keydown", handlePhoneKeydown);
+    } else {
+        inputPhone.removeEventListener("input", handlePhoneInput);
+        inputPhone.removeEventListener("keydown", handlePhoneKeydown);
+    }
+    updateFormState();
+});
 
 let typingTimer;
-const typingDelay = 500;
-
-document.addEventListener("DOMContentLoaded", function () {
-    if (countrySelect.value === "RU") {
-        phoneInput.value = "+7";
-        phoneInput.addEventListener("input", handlePhoneInput);
-        phoneInput.addEventListener("keydown", handlePhoneKeydown);
-    }
-
-    countrySelect.addEventListener("change", function () {
-        resetValidationError();
-
-        if (countrySelect.value === "RU") {
-            phoneInput.value = "+7";
-            phoneInput.addEventListener("input", handlePhoneInput);
-            phoneInput.addEventListener("keydown", handlePhoneKeydown);
-        } else {
-            phoneInput.value = "";
-            phoneInput.removeEventListener("input", handlePhoneInput);
-            phoneInput.removeEventListener("keydown", handlePhoneKeydown);
-        }
-    });
-});
 
 function handlePhoneInput() {
     clearTimeout(typingTimer);
-    let value = phoneInput.value.replace(/\D/g, "");
+    let value = inputPhone.value.replace(/\D/g, "");
 
     if (value.length > 0) {
         if (value.startsWith("8")) {
@@ -161,12 +275,13 @@ function handlePhoneInput() {
         }
 
         value = value.slice(0, 11);
-        phoneInput.value = formatPhoneNumber(value);
+        inputPhone.value = formatPhoneNumber(value);
     }
 
     typingTimer = setTimeout(() => {
-        validatePhone(value);
-    }, typingDelay);
+        validatePhone(value, countrySelect.value);
+        updateFormState();
+    }, 500);
 }
 
 function handlePhoneKeydown(event) {
@@ -174,7 +289,7 @@ function handlePhoneKeydown(event) {
         event.preventDefault();
     }
 
-    if (phoneInput.value.replace(/\D/g, "").length >= 11 && event.key !== "Backspace" && event.key !== "Delete") {
+    if (inputPhone.value.replace(/\D/g, "").length >= 11 && event.key !== "Backspace" && event.key !== "Delete") {
         event.preventDefault();
     }
 }
@@ -194,185 +309,82 @@ function formatPhoneNumber(value) {
     return formatted;
 }
 
-function validatePhone(value) {
-    let errorElement = document.querySelector(".phone-error");
-    const wrapper = phoneInput.closest('.modal__wrapper');
-    if (!wrapper) return;
+function validatePhone(value, country) {
+    let errorElement = document.querySelector(".error-message");
+    const wrapper = inputPhone.closest('.modal__wrapper');
+    if (!wrapper) return false;
 
     if (!errorElement) {
         errorElement = document.createElement("span");
-        errorElement.className = "phone-error";
+        errorElement.className = "error-message";
         wrapper.parentNode.insertBefore(errorElement, wrapper.nextSibling);
     }
 
-    const operatorCodes = [
-        "904", "910", "911", "912", "913", "914", "915", "916", "917", "918", "919",
-        "920", "921", "922", "923", "924", "925", "926", "927", "928", "929",
-        "930", "931", "932", "933", "934", "935", "936", "937", "938", "939",
-        "950", "951", "952", "953", "954", "955", "956", "957", "958", "959",
-        "960", "961", "962", "963", "964", "965", "966", "967", "968", "969",
-        "970", "971", "972", "973", "974", "975", "976", "977", "978", "979",
-        "980", "981", "982", "983", "984", "985", "986", "987", "988", "989",
-        "991", "992", "993", "994", "995", "996", "997", "998", "999"
-    ];
-
     let errorMessage = "";
 
-    if (!/^\d+$/.test(value)) {
-        errorMessage = "Invalid phone number.";
-    } else if (value.length < 11) {
-        errorMessage = "Invalid phone number.";
-    } else {
-        const cityCode = value.substring(1, 4);
-        if (!operatorCodes.includes(cityCode)) {
+    if (country === "RU") {
+        const operatorCodes = [
+            "900", "904", "910", "911", "912", "913", "914", "915", "916", "917", "918", "919",
+            "920", "921", "922", "923", "924", "925", "926", "927", "928", "929",
+            "930", "931", "932", "933", "934", "935", "936", "937", "938", "939",
+            "950", "951", "952", "953", "954", "955", "956", "957", "958", "959",
+            "960", "961", "962", "963", "964", "965", "966", "967", "968", "969",
+            "970", "971", "972", "973", "974", "975", "976", "977", "978", "979",
+            "980", "981", "982", "983", "984", "985", "986", "987", "988", "989",
+            "991", "992", "993", "994", "995", "996", "997", "998", "999"
+        ];
+
+        if (!/^\d+$/.test(value)) {
             errorMessage = "Invalid phone number.";
+        } else if (value.length < 11) {
+            errorMessage = "Invalid phone number.";
+        } else {
+            const cityCode = value.substring(1, 4);
+            if (!operatorCodes.includes(cityCode)) {
+                errorMessage = "Invalid phone number.";
+            }
         }
+    } else if (!country) {
+        errorMessage = "This field is required.";
+    } else if (!value.trim()) {
+        errorMessage = "This field is required."; 
     }
 
     if (errorMessage) {
-        addError(phoneInput, errorMessage);
+        addError(wrapper, errorMessage);
+        return false;
     } else {
-        removeError(phoneInput);
+        removeError(wrapper);
+        return true;
     }
-
-    return !errorMessage;
 }
 
 function resetValidationError() {
-    let errorElement = document.querySelector(".phone-error");
+    let errorElement = document.querySelector(".error-message");
     if (errorElement) {
         errorElement.textContent = "";
     }
 }
 
-document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    validateForm();
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault(); 
 
-    const phoneValue = phoneInput.value.replace(/\D/g, "");
-    if (countrySelect.value === "RU" && validatePhone(phoneValue)) {
-        if (validateForm()) {
-            const submitBtn = document.querySelector(".modal__window__form-btn");
-            submitBtn.classList.add('active');
-        } else {
-            const submitBtn = document.querySelector(".modal__window__form-btn");
-            submitBtn.classList.remove('active');
-        }
-    } else {
-        const submitBtn = document.querySelector(".modal__window__form-btn");
-        submitBtn.classList.remove('active');
-    }
-});
+    isNameTouched = true;
+    isEmailTouched = true;
+    isPhoneTouched = true;
 
-// Валидация формы
-function validateForm() {
-    let isValid = true;
+    const isFormValid = updateFormState();
 
-    const requiredFields = document.querySelectorAll('.modal__span');
-
-    requiredFields.forEach(span => {
-        const label = span.closest('.modal__label');
-        const input = label.querySelector('.modal__input');
-
-        if (!input.value.trim()) {
-            addError(input, 'This field is required');
-            isValid = false;
-        } else {
-            removeError(input);
-        }
-    });
-
-    return isValid;
-}
-
-
-function addError(input, message) {
-    removeError(input);
-
-    const wrapper = input.closest('.modal__wrapper');
-    if (!wrapper) return;
-
-    wrapper.classList.add('error-border'); // Add error border
-
-    let existingError = wrapper.parentNode.querySelector('.error-message');
-    if (!existingError) {
-        const errorElement = document.createElement('span');
-        errorElement.className = 'error-message';
-        errorElement.textContent = message;
-
-        wrapper.parentNode.insertBefore(errorElement, wrapper.nextSibling);
-    }
-
-    input.classList.add('invalid'); // Mark the input as invalid
-
-    input.addEventListener('input', function handler() {
-        removeError(input);
-        input.removeEventListener('input', handler);
-    });
-}
-
-function removeError(input) {
-    input.classList.remove('invalid'); // Remove the invalid class
-
-    const wrapper = input.closest('.modal__wrapper');
-    if (!wrapper) return;
-
-    // Remove error border
-    wrapper.classList.remove('error-border');
-
-    const errorElement = wrapper.parentNode.querySelector('.error-message');
-    if (errorElement) {
-        errorElement.remove(); // Remove the error message
-    }
-}
-
-
-
-const requiredInputs = document.querySelectorAll('.modal__input');
-
-requiredInputs.forEach(input => {
-    input.addEventListener('input', () => {
-        if (input.value.trim()) {
-            removeError(input);
-        }
-    });
-});
-
-function toggleSubmitButton() {
-    const submitBtn = document.querySelector(".modal__window__form-btn");
-    const phoneValue = phoneInput.value.replace(/\D/g, "");
-
-    if (countrySelect.value === "RU" && validatePhone(phoneValue) && validateForm()) {
-        submitBtn.classList.add('active');
-    } else {
-        submitBtn.classList.remove('active');
-    }
-}
-
-requiredInputs.forEach(input => {
-    input.addEventListener('input', toggleSubmitButton);
-});
-phoneInput.addEventListener('input', toggleSubmitButton);
-
-
-
-
-
-const submitBtn = document.querySelector(".modal__window__form-btn");
-const modalThanks = document.querySelector(".modal-thanks");
-const thanksClose = document.querySelector(".modal-thanks__close-btn");
-const thanksMainBtn = document.querySelector(".modal-thanks__btn");
-
-submitBtn.addEventListener("click", () => {
-    if (submitBtn.classList.contains('active')) {
+    if (isFormValid) {
         modalThanks.classList.add('show-modal-thanks');
         modalBackground.style.display = "none";
 
         submitBtn.classList.remove('active');
         bodyElementHTML.style.marginRight = "-" + scrollbarWidth + "px";
+    } else {
+        console.log('Форма невалидна, исправьте ошибки');
     }
 });
-
 
 thanksClose.addEventListener("click", () => {
     modalThanks.classList.remove('show-modal-thanks');
